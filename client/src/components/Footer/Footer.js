463a353b54d58048ab9b071bb9d5d983c18339ca
Lyubomir <lyubomir_vasilev1992@abv.bg>
2021-04-01 15:10:29 +0300

    Style the profile page for user profiles

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,6 +7,7 @@ function Footer() {
         <p>
             Smart Fox &copy;  ALL RIGHTS RESERVED.  | DESIGN BY <a href="https://xenodochial-blackwell-6396b1.netlify.app/#the-begining">Smart Fox Design</a>.
         </p>
+        <p className="site-version">0.1v</p>
        </footer>
     );
 }
--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,19 +1,37 @@
 import "./Profile.css";
 
+import { useAuth } from "../Authentication/AuthContext";
+
+
 
 export default function Profile() {
+
+    const { currentUser } = useAuth();
+
     return (
         <div className="profile">
-            
+            <div className="profile-wrapper">
+                <div className="profile-img">
+                    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png" />
+                    <div className="btn-wrapper">
+                    <button>Upload photo</button>
+                    <button>Select avatar</button>
+                    </div>
+               
+                </div>
+                <div className="profile-info">
+                    <h1>Lyubodos</h1>
+                    <p class="profile-title">E-mail: {currentUser.email}</p>
+                    <p>Status: Member</p>
+                </div>
+            </div>
 
-                <h1>Lyubodos</h1>
-                <p class="profile-title">Title:</p>
-                <p>Occupation:</p>
-                <a href="#"><i class="fa fa-dribbble"></i></a>
-                <a href="#"><i class="fa fa-twitter"></i></a>
-                <a href="#"><i class="fa fa-linkedin"></i></a>
-                <a href="#"><i class="fa fa-facebook"></i></a>
-                <p><button>Contact</button></p>
+            <div className="profile-favourites">
+                <h3>Favourite Games:</h3>
+                <div className="favourites-wrapper">
+
+                </div>
+            </div>
         </div>
     )
 }
